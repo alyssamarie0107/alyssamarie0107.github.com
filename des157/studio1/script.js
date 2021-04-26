b642afb2efc78d5a068a215b531545db828067d2
alyssa rodriguez <amrrodriguez@ucdavis.edu>
2021-04-26 16:08:15 -0700

    finished output scripts

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -12,7 +12,7 @@
 
     // function that handles curving the welcome text
     // referenced: https://www.youtube.com/watch?v=a0FIA_qSVLs
-    function curveWelcomeText () {
+    function curveWelcomeText() {
         const radius = 250;
         const diameter = radius * 2;
 
@@ -39,8 +39,8 @@
             // put char into span element
             spanElement.innerHTML = char;
 
-            const xPos = radius * (1+ Math.cos(angleToRadians(angle)));
-            const yPos = radius * (1+ Math.sin(angleToRadians(angle)));
+            const xPos = radius * (1 + Math.cos(angleToRadians(angle)));
+            const yPos = radius * (1 + Math.sin(angleToRadians(angle)));
 
             angle += deltaAngle;
 
@@ -54,7 +54,7 @@
             circle.appendChild(spanElement);
         });
     }
-    
+
     curveWelcomeText();
 
     // -----------------------
@@ -63,9 +63,10 @@
     const form = document.querySelector('form');
     const output = document.querySelector('#output');
     let outputText = document.querySelector('#output-text');
+    let affirmation = document.querySelector('#affirmation');
 
     // handles when form is submitted
-    form.addEventListener('submit', function(event){
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
 
         form.style.display = "none";
@@ -75,11 +76,9 @@
         const fname = document.querySelector('#fname').value;
         console.log("fname: " + fname);
 
-        const feelings = document.querySelectorAll('input[type="checkbox"]:checked');
-        for(let feeling of feelings) {
-            console.log(feeling.value);
-        }
-    
+        const feeling = document.querySelector('input[type="radio"]:checked').value;
+        console.log("feeling: " + feeling);
+
         const hobby = document.querySelector('#hobby').value;
         console.log("hobby: " + hobby);
 
@@ -90,16 +89,88 @@
         console.log("person: " + person);
 
         // generate madlibs output
-        outputText.innerHTML = 
-        `
-        Hello ${fname}!
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        `;
+        if (feeling == "happy") {
+            // happy script
+            outputText.innerHTML =
+                `
+            Hello ${fname.bold()}! Welcome to Mood Affirmation MadLibs, where you are given positive reminders or statements
+            based on your mood. It is great that you are taking the time to hone into how you are feeling today. 
+            Thank you for setting some time out of your busy day and for sharing a little about yourself. You mentioned
+            that you are feeling ${feeling.bold()} today. That is awesome, for you are worthy of being happy! 
+            To keep the happiness up, try doing some of the things you love and that make you happy, such as ${hobby.bold()},
+            indulging in ${food.bold()}, or simply hanging out with the person who makes you the happiest, ${person.bold()}. 
+            There is a lot in life to be happy about and it is great news to hear that you are. Keep it up :)
+            `;
+
+            affirmation.innerHTML = 'Affirmation: "Every day I have more and more reasons to be happy and joyful. - I am"';
+        } else if (feeling == "sad") {
+            // sad script
+            outputText.innerHTML =
+                `
+            Hello ${fname.bold()}! Welcome to Mood Affirmation MadLibs, where you are given positive reminders or statements
+            based on your mood. It is great that you are taking the time to hone into how you are feeling today. 
+            Thank you for setting some time out of your busy day and for sharing a little about yourself. You mentioned
+            that you are feeling ${feeling.bold()} today. I am sorry to hear that. I hope you know that it is okay to feel like
+            this. Though it may not seem like it right now, things are going to be okay and better. Allow yourself to feel the way 
+            you do, but how about doing some of the things you know you love and make you smile, such as ${hobby.bold()}, indulging 
+            in ${food.bold()}, or simply hanging out with the person who makes you the happiest, ${person.bold()}. There is no rush 
+            to feel better. These are just some reminders and suggestions. Do everything on your own time and if things do not work 
+            out today, tomorrow will be better. Remember, this feeling is only temporary. You've got this.
+            `;
+
+            affirmation.innerHTML = 'Affirmation: "Difficult times are part of my journey and allow me to appreciate the good." - I am';
+        } else if (feeling == "anxious") {
+            // anxious and overwhelmed script
+            outputText.innerHTML =
+                `
+            Hello ${fname.bold()}! Welcome to Mood Affirmation MadLibs, where you are given positive reminders or statements
+            based on your mood. It is great that you are taking the time to hone into how you are feeling today. 
+            Thank you for setting some time out of your busy day and for sharing a little about yourself. You mentioned that you are 
+            feeling ${feeling.bold()} today. First, take a deep breath and try your best to step back from everything that is making
+            you feel anxious. Remind yourself that you are stronger than your anxious thoughts. If you need someone, how about you 
+            reaching out to ${person.bold()}? Once you feel at ease, how about doing some of your favorite things, such as 
+            ${hobby.bold()} or indulging in ${food.bold()}. This feeling will pass. You're okay.
+            `;
+
+            affirmation.innerHTML = 'Affirmation: "Be gentle with yourself, you are doing the best you can." - I am';
+
+        } else if (feeling == "angry") {
+            // angry script
+            outputText.innerHTML =
+                `
+            Hello ${fname.bold()}! Welcome to Mood Affirmation MadLibs, where you are given positive reminders or statements
+            based on your mood. It is great that you are taking the time to hone into how you are feeling today. 
+            Thank you for setting some time out of your busy day and for sharing a little about yourself. You mentioned that you are 
+            feeling ${feeling.bold()} today. I'm sure there is a valid reason for this. Being angry is human. However, challenge yourself
+            to control the way you respond to what is making you feel this way. Take a breather, go out and do some of your favorite things, 
+            such as ${hobby.bold()}, indulging in ${food.bold()}, or hanging out with the person who makes you the happiest, ${person.bold()}.
+            Then come back and check in with yourself to see if it is worth staying angry.
+            `;
+
+            affirmation.innerHTML = 'Affirmation: Let things flow and be positive. - I am';
+
+        } else {
+            // overwhelmed script
+            outputText.innerHTML =
+                `
+            Hello ${fname.bold()}! Welcome to Mood Affirmation MadLibs, where you are given positive reminders or statements
+            based on your mood. It is great that you are taking the time to hone into how you are feeling today. 
+            Thank you for setting some time out of your busy day and for sharing a little about yourself. You mentioned that you are 
+            feeling ${feeling.bold()} today. That is okay! Life can be very hectic and it is only normal to feel like this when it is.
+            I am sure you have a lot going on right now and you may not be entirely sure how you are going to get everything sorted out. 
+            But take a breath and trust in yourself. You don't have to tackle everything all at once. Do one things at a time and trust
+            that you will figure out the rest along the way. It is important though, that you take a break from whatever it is that is 
+            overwhelming you. Take some time and do some of your favorite things, such as ${hobby.bold()}, indulging in ${food.bold()}, or 
+            hanging out with the person who makes you the happiest, ${person.bold()}. Everything will be okay and will work out. 
+            `;
+
+            affirmation.innerHTML = 'Affirmation: "It is okay to take time for myself." - I am';
+        }
     });
 
     const resetBtn = document.querySelector('#reset-btn');
-    
-    resetBtn.addEventListener('click', function(){
+
+    resetBtn.addEventListener('click', function () {
         form.style.display = "block";
         output.style.display = "none";
 
